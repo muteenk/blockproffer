4f44d920ab3c5ae0a252856f577c26fb5f01bbe9
MuteenK <kundangarmuteen@protonmail.com>
2023-01-23 02:29:16 +0530

    connected client and server

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+// Modules
+const express = require("express")
+const http = require("http");
+const path = require('path')
+const {Server} = require("socket.io")
+const cors = require("cors");
+
+
+// Configuration
+const app = express();
+const port = 5555 || process.env.port
+const server = http.createServer(app);
+const io = new Server(server);
+// app.set('view engine', "ejs");
+// app.use('/static', express.static(path.join(__dirname, '../public')))
+
+
+app.use(cors({
+    origin: "http://localhost:3000"
+})); 
+app.use(express.json());
+
+
+
+// End Points
+app.get('/:room', (req, res) => {
+    res.status(200).send({'Hello': req.params.room});
+});
+
+
+
+// Socket Listener
+io.on("connection", (socket) => {
+    console.log('New User Connected -> ' + socket.id );
+    socket.on("disconnect", () => {
+        console.log("User Disconnected")
+    })
+    socket.on("message", (msg) => {
+        socket.broadcast.emit("message", msg);
+    })
+});
+
+
+// Server
+server.listen(port, () => {
+    console.log(`Serving at http://127.0.0.1:${port}`);
+})
--- frontend/src/components/livepolls/Livepolls.jsx
+++ frontend/src/components/livepolls/Livepolls.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
 import HeaderWithBackButton from '../header/HeaderWithBackButton'
+import './Livepolls.css'
+
 
 function Livepolls() {
+
+  const [roomName, changeRoomName] = useState("")
+
+
+  function changeRoomData(e) {
+    changeRoomName(e.target.value);
+    console.log(e.target.value)
+  }
+
+
+  async function sendRoomReq(){
+    const URL=`http://localhost:5555/${roomName}`;
+    const Res= fetch(URL);
+    const response= await Res;
+    const json= await response.json();
+    console.log(json);
+  }
+
   return (
-    <div>
-        <HeaderWithBackButton />
-    </div>
+    <>
+      <HeaderWithBackButton />
+
+      <div className="room-sheduler">
+        <h1>Enter Room ID: </h1>
+
+        <form>
+          <input type="text" onChange={changeRoomData} value={roomName} placeholder='Enter room ID'/>
+          <button type='button' onClick={sendRoomReq}>Enter</button>
+        </form>
+      </div>
+    </>
   )
 }
 
