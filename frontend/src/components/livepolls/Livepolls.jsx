b1b8efcb3de7b637500ec2dc91b494f3bc4fb90f
MuteenK <kundangarmuteen@protonmail.com>
2023-01-23 03:35:27 +0530

    dummy room system

--- frontend/src/components/livepolls/Livepolls.jsx
+++ frontend/src/components/livepolls/Livepolls.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useState } from 'react';
 import HeaderWithBackButton from '../header/HeaderWithBackButton'
+import Pollquestion from '../pollquestion/Pollquestion'
 import './Livepolls.css'
 
 
 function Livepolls() {
 
   const [roomName, changeRoomName] = useState("")
+
+  const [room, changeRoom] = useState(null);
+
+  const [roomNotFound, changeRoomNotFound] = useState(false);
 
 
   function changeRoomData(e) {
@@ -15,26 +20,44 @@ function Livepolls() {
   }
 
 
-  async function sendRoomReq(){
+  async function sendRoomReq(e){
+    e.preventDefault();
     const URL=`http://localhost:5555/${roomName}`;
     const Res= fetch(URL);
     const response= await Res;
-    const json= await response.json();
-    console.log(json);
+    if (response.status == 200){
+      const json= await response.json();
+      console.log(json);
+      changeRoom(1);
+      changeRoomNotFound(false);
+    }
+    else{
+      changeRoomNotFound(true);
+    }
+    
   }
 
   return (
     <>
+
+    {(room == null) ? 
+    <>
       <HeaderWithBackButton />
 
+
       <div className="room-sheduler">
+
+      {(roomNotFound == true) ? <><h4 className='notFound'>Room Not Found !</h4></> : ""}
+
         <h1>Enter Room ID: </h1>
 
-        <form>
-          <input type="text" onChange={changeRoomData} value={roomName} placeholder='Enter room ID'/>
-          <button type='button' onClick={sendRoomReq}>Enter</button>
+        <form onSubmit={sendRoomReq}>
+          <input type="text" onChange={changeRoomData} value={roomName} placeholder='Enter room ID' required/>
+          <button>Enter</button>
         </form>
       </div>
+    </> : <Pollquestion/>}
+      
     </>
   )
 }
