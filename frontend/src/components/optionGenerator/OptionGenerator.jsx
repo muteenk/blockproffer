8534bce4821e825025818d3a6cd74c5c9f901cb3
MuteenK <kundangarmuteen@protonmail.com>
2023-01-24 22:21:13 +0530

    Made Options and form Functional

--- frontend/src/components/optionGenerator/OptionGenerator.jsx
+++ frontend/src/components/optionGenerator/OptionGenerator.jsx
@@ -1,37 +1,37 @@
 import React from 'react'
 import { useState } from 'react'
+import './optionGenerator.css'
+import Option from './Option'
 
-const OptionGenerator = () => {
-
-    const [options, setOptions] = useState(0)
-    const [optionValues, setOptionValues] = useState([]);
-
-
+const OptionGenerator = (props) => {
+    
     const [optionInput, setOptionInput] = useState('')
+    // const [optionValues, setOptionValues] = useState([]);
 
     const optionHandler = (e) => {
         setOptionInput(e.target.value)
     }
 
-
     const addOption = () => {
-        setOptions(options + 1)
-        setOptionValues([...optionValues, optionInput]);
+        // setOptionValues([...optionValues, optionInput]);
+        props.setOptions([...props.options, optionInput]);
+        setOptionInput('');
     }
 
-    const deleteOption = (index) => {
-        setOptionValues(optionValues.filter((optionValues, i) => {
-            if (i !== index) {
-                return optionValues;
-            }
+    const deleteOption = (option) => {
+        props.setOptions(props.options.filter(e => {
+            return e !== option;
         }))
     }
 
   return (
     <div className='optionGenerator'>
+
+        <h3>Add Options</h3>
+
         <div id="options">
-            {optionValues.map((optionValues, index) => {
-                return <span key={index}>{optionValues} <a id={index} onClick={(e) => {deleteOption(e.target.id)}}><i class="fa-solid fa-circle-xmark"></i></a></span>
+            {props.options.map((e, index) => {
+                return <Option key={index} data={e} deleteOption={deleteOption}/>
             })}
         </div>
         <input type="text" id="optionInput" onChange={optionHandler} value={optionInput} required/>
