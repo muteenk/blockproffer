ad20bbb769c8ed9c82369ee1b5102d81697c54d9
vehehgito <aryann2708@gmail.com>
2023-01-25 10:19:06 +0530

    Idk what

--- backend/solidity/hardhat.config.js
+++ backend/solidity/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ganache");
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+module.exports = 
+{
   solidity: "0.8.17",
+  network:
+  {
+    ganache: 
+    {
+      url: '',
+      accounts: ['c7d2bbc15215910b453bb63dbf0067eb42a943e1751e3608b53daa8ef669be97'],
+      
+    }
+  }
+
 };
