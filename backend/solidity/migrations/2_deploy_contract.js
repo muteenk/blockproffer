ea77caf0f9cb21a09460d539a13afec572830a2a
vehehgito <aryann2708@gmail.com>
2023-01-26 16:02:40 +0530

    Configured deployment script migrations/2_deploy_contract.js

--- backend/solidity/migrations/2_deploy_contract.js
+++ backend/solidity/migrations/2_deploy_contract.js
@@ -2,5 +2,5 @@ var pollSystem = artifacts.require("pollSystem");
 
 module.exports = function(deployer)
 {
-    deployer.deploy(pollSystem);
+    deployer.deploy(pollSystem, ['0x287A61aeF44dca6e774A9730F7BcE386a509AB85']); //Feeds contract identifier and addresses of the option/delegates
 };
