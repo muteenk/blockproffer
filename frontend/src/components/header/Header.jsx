a4260df177e7d06fa73f53e58fe57abcd443bf13
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-01-26 23:17:14 +0530

    Responsive Desgin Started

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './header.css'
 import { NavLink } from 'react-router-dom'
 import logo from '../../assets/blockproffer-logo.png'
 import '../../App.css'
 
 function Header() {
+
+    const [burger_class, setBurgerClass] = useState('burger-bar unclicked')
+    const [menu_class, setMenuClass] = useState('menu hidden')
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    const updateMenu = () => {
+        if(!isMenuClicked)
+        {
+            setBurgerClass('burger-menu clicked')
+            setMenuClass('menu visible')
+        }
+        else
+        {
+            setBurgerClass('burger-menu unclicked')
+            setMenuClass('menu hidden')
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
   return (
+    <>
     <nav>
+        <div className='burger-menu' onClick={updateMenu}>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
+            <div className={burger_class}></div>
+        </div>
         <div className='logo'>
             <NavLink to="/">
                 <img src={logo} alt="Text-And-Cube-Logo" />
@@ -37,6 +61,8 @@ function Header() {
             </NavLink>
         </div>
     </nav>
+    <div className={menu_class}></div>
+    </>
   )
 }
 
