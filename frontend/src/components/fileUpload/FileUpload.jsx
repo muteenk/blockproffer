819f8c92bd33b2942340946306ea2b096aab9673
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-01-26 13:51:02 +0530

    Upload UI Updated

--- frontend/src/components/fileUpload/FileUpload.jsx
+++ frontend/src/components/fileUpload/FileUpload.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import './fileUpload.css'
 
 function FileUpload(props) {
@@ -7,7 +6,7 @@ function FileUpload(props) {
 
   return (
     <>
-        <div className='file-card'>
+        {/* <div className='file-card'>
             <p className="file-err">
                 {props.fileError}
             </p>
@@ -15,6 +14,7 @@ function FileUpload(props) {
                 {props.file.name}
             </p>
             <div className='file-inputs'>
+                <label>Upload a CSV file to get the name of the eligible voters</label>
                 <input type='file' id='file' name='csv-file' onChange={props.handleFileParse} required/>
                 <button>
                     <i className='fas fa-upload'></i>
@@ -24,6 +24,34 @@ function FileUpload(props) {
 
             <p className='main'>Support Files</p>
             <p className='info'>CSV</p>
+        </div>
+         */}
+
+        <div className='file-card'>
+            <p className="file-err">
+                {props.fileError}
+            </p>
+            <div className='file-inputs'>
+                <label>Upload a CSV file to get the name of the eligible voters</label>
+                <input type='file' id='file' name='csv-file' onChange={props.handleFileParse} required></input>
+                <i className='fas fa-upload'></i>
+                <h1>
+                    Upload
+                </h1>
+            </div>
+        </div>
+        <div className="progress-area">
+                <li className='row'>
+                    <i className="fas fa-file-alt"></i>
+                    <div className="details">
+                        <span className="file-name">{props.file.name}</span>
+                    </div>
+                    {/* <div className='delete-btn'>
+                        <button>
+                            <i className="fa-solid fa-trash"></i>
+                        </button>
+                    </div> */}
+                </li>
         </div>
     </>
   )
