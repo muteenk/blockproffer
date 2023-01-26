ed9ae776555bb3ec8f131112022caed88967a3e1
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2023-01-26 12:57:53 +0530

    Revert "fixed option bug"
    This reverts commit e5177601ecaf66740c2348f17d49be352c7526c1.

--- frontend/src/components/optionGenerator/Option.jsx
+++ frontend/src/components/optionGenerator/Option.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Option = (props) => {
   return (
     <>
-        <span className='option' key={props.index}>
+        <span className='option'>
           <span className='option-name'>
             {props.data}
           </span>
-          <button type="button" onClick={() => {props.deleteOption(props.i)}}>
+          <button type="button" onClick={() => {props.deleteOption(props.data)}}>
               <i class="fa-solid fa-circle-xmark"></i>
           </button>
         </span>
