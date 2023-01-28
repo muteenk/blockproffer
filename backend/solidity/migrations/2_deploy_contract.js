c57bf86e6fc3e891ac1bfc75bd296e4cdf4f3c12
vehehgito <aryann2708@gmail.com>
2023-01-28 21:56:43 +0530

    Nabi

--- backend/solidity/migrations/2_deploy_contract.js
+++ backend/solidity/migrations/2_deploy_contract.js
@@ -1,6 +1,7 @@
 var pollSystem = artifacts.require("pollSystem");
+var listOptions = ['0x287A61aeF44dca6e774A9730F7BcE386a509AB85']
 
 module.exports = function(deployer)
 {
-    deployer.deploy(pollSystem, ['0x287A61aeF44dca6e774A9730F7BcE386a509AB85']); //Feeds contract identifier and addresses of the option/delegates
+    deployer.deploy(pollSystem, listOptions); //Feeds contract identifier and addresses of the option/delegates
 };
