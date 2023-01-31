88b0911e8f12c55f03a8c0da8090ff059766149b
MuteenK <kundangarmuteen@protonmail.com>
2023-01-31 17:01:04 +0530

    User Token Verification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import Livepolls from './components/livepolls/Livepolls';
 import Pollquestion from './components/pollquestion/Pollquestion';
 import HowItWorks from './components/home/HowItWorks';
 import Success from './components/createpoll/Success';
-import Verification from './components/livepolls/Verification';
 
 
 function App() {
@@ -24,7 +23,6 @@ function App() {
         <Route path="/pollquestion" element={<Pollquestion />} />
         <Route path="/howitworks" element={<HowItWorks />} />
         <Route path="/success" element={<Success />} />
-        <Route path="/verification" element={<Verification />} />
         <Route path="*" element={<PNF />} />
       </Routes>
     </>
