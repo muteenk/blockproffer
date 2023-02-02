f55bf0eb2eeab1543de48f5a721487538417b876
MuteenK <kundangarmuteen@protonmail.com>
2023-02-02 22:43:02 +0530

    Timer Function updated

--- frontend/src/components/pollquestion/Questions.jsx
+++ frontend/src/components/pollquestion/Questions.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { PollContext } from '../../Helpers/Contexts'
 import Header from '../header/Header';
+import { useCountdown } from '../Timer/CountDown';
 import Timer from '../Timer/Timer';
 
 function Questions(props) {
@@ -70,89 +71,55 @@ function Questions(props) {
     const endDate = props.roomData.endDate;
     const endTime = props.roomData.endTime;
 
+    const [days, hours, minutes, seconds] = useCountdown(endDate + " " + endTime);
 
-    const [timerDays, setTimerDays] = useState()
-    const [timerHours, setTimerHours] = useState()
-    const [timerMinutes, setTimerMinutes] = useState()
-    const [timerSeconds, setTimerSeconds] = useState()
-
-    let interval;
-    const startTimer = () => {
-        const countdownDate = new Date(endDate + " " + endTime);
-
-        interval = setInterval(() => {
-            const now = new Date();
-            const distance = Math.abs(countdownDate - now);
-
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            if (distance <= 0) {
-                // stop our timer
-                clearInterval(interval.current);
-                setQuestion("finalScreen")
-            } else {
-                // update timer
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-        }, 1000)
-
+    if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+        setQuestion("finalScreen")
     }
+    else{
+        return (
 
-    // componentDidMount
-    useEffect(() => {
-        startTimer();
-    }, [])
-    
-    return (
+            <>
+            <Header />
+            <div class='bg-gray-900 h-screen w-full flex flex-col align-center justify-center'>
+                <div class='flex justify-center align-center'>
+                    <div class='flex justify-center align-center w-8/12'>
+                        <div class='flex flex-col w-8/12 align-center justify-items-center'>
+                            <div class="pb-[2rem]">
+                                <Timer timerDays={days} timerHours={hours} timerMinutes={minutes} timerSeconds={seconds}/>
+                            </div>
+                            <h3 class="mb-4 text-center text-6xl font-semibold text-gray-900 dark:text-white">{props.roomData.title}</h3>
+                            <p class="mb-4 text-center text-xl font-medium text-gray-900 dark:text-white">{props.roomData.description}</p>
+                            <ul class="w-full flex justify-center flex-col text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                
+                                {props.roomData.pollOptions.map((option, index) => {
+                                    return ( 
+                                    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id={"list-radio" + option.optionNum} type="radio" onChange={selectOption} value={option.optionNum} name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                        <label for={"list-radio" + option.optionNum} class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{option.option}</label>
+                                    </div>
+                                    </li>   
+                                    )
+                                })}
 
-        <>
-        <Header />
-        {(props.userVoted) ? setQuestion("endScreen") : 
-        <div class='bg-gray-900 h-screen w-full flex flex-col align-center justify-center'>
-            <div class='flex justify-center align-center'>
-                <div class='flex justify-center align-center w-8/12'>
-                    <div class='flex flex-col w-8/12 align-center justify-items-center'>
-                        <div>
-                            <Timer timerDays={timerDays} timerHours={timerHours} timerMinutes={timerMinutes} timerSeconds={timerSeconds}/>
+                            </ul>
                         </div>
-                        <h3 class="mb-4 text-center text-6xl font-semibold text-gray-900 dark:text-white">{props.roomData.title}</h3>
-                        <p class="mb-4 text-center text-xl font-medium text-gray-900 dark:text-white">{props.roomData.description}</p>
-                        <ul class="w-full flex justify-center flex-col text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                            
-                            {props.roomData.pollOptions.map((option, index) => {
-                                return ( 
-                                <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-                                <div class="flex items-center pl-3">
-                                    <input id={"list-radio" + option.optionNum} type="radio" onChange={selectOption} value={option.optionNum} name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                    <label for={"list-radio" + option.optionNum} class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{option.option}</label>
-                                </div>
-                                </li>   
-                                )
-                            })}
-
-                        </ul>
+                    </div>
+                </div>
+                <div class='flex justify-center'>
+                    <div class='grid justify-items-end mt-4 mb-4 w-2/5'>
+                        <button onClick={addVote} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                Submit
+                            </span>
+                        </button>
                     </div>
                 </div>
             </div>
-            <div class='flex justify-center'>
-                <div class='grid justify-items-end mt-4 mb-4 w-2/5'>
-                    <button onClick={addVote} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-                        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                            Submit
-                        </span>
-                    </button>
-                </div>
-            </div>
-        </div>
-}
-        </>
-    )
+            </>
+        )
+        }
 }
 
 export default Questions
