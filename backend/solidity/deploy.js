e12c07bcd3d1a42d6dc8eea115da3b1a4795c8aa
vehehgito <aryann2708@gmail.com>
2023-02-02 21:35:23 +0530

    Changed deploy env from truffle to hardhat

---
+++ backend/solidity/deploy.js
@@ -0,0 +1,9 @@
+const { ethers } = require('hardhat')
+const fs = require('fs')
+
+async function main() 
+{
+  const base_uri = ''
+  const Contract = await ethers.getContractFactory('pollSystem')
+  
+}
